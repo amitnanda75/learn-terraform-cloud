75666e0321543c3933c44463933e14586bbeb980
Amit Nanda <amitnanda@hotmail.com>
2024-04-01 18:07:57 -0400

    Remove cloud block
    	modified:   terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "Amit-AWS-Dev-Org"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
